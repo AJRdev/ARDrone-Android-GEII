63b35f4d0124e8e59c6db74756e359629de6d3c5
AJRdev <andre.ranarivelo@gmail.com>
2015-06-07 21:47:01 +0100

    Add Javadoc comments to GereDrone class

--- src/com/project/ardrone_android_geii/GereDrone.java
+++ src/com/project/ardrone_android_geii/GereDrone.java
@@ -17,40 +17,42 @@ import android.widget.Toast;
 
 
 
+/**
 
+ * Class who handle communication with the Drone (UDP Protocols and AT Commands)
+
+ */
 public class GereDrone {
-	
-	
+
+
 	ScheduledExecutorService mScheduleTaskExecutor = Executors.newScheduledThreadPool(2);
-	
+
 	public DatagramPacket udp_packet;
 	public DatagramSocket udp_socket;
 	public InetAddress adresse;
 	public JoyActivityVideo mJoy;
-	
-	
+
+
 	GereThreads mThreads;
 	NavdataReceiver NavRec;
-	
-	//public DatagramSocket sauv_Socket;
-	//boolean bSocketClose = true;
-	
+
+
 	public byte[] buffer;
 
 
-	
+
 	String CommandeGenere = "";
 	String sAdresse;
 	String sTemps;
-	
-	
+
+
 	static int iPitch = 0;
 	static int iAltitude = 0;
 	static int iBatterie = 0;
 	static int iYaw = 0;
 	static int iRoll = 0;
 	static int iSpeed = 0;
-	
+
 	int iVal = 0;
 	int iPortDrone;
 
@@ -60,186 +62,253 @@ public class GereDrone {
 	int iMinuteD = 0;
 	int icSecondeU = 0;
 	int icSecondeD = 0;
-	
+
 	public GereDrone(){
-		
+
 	}
-	
+
+	/**
+
+	 *	This constructor receives as parameter the address and port of the drone 
+
+		to instantiate and a command to send. 
+
+		It instantiates GereThreads class that manages most of the necessary 
+
+		threads in the application and NavdataReceiver class that get drone status and 
+
+		data.
+
+		It also creates the socket and packet with CreationSocketPacket () 
+
+		function, and starts immediatly WatchDog thread to avoid losing the 
+
+		connection.
+
+	 */
 	public GereDrone(String CommandeStart, String Adresse, int iPort, JoyActivityVideo joyActivity){			// CONSTRUCTEUR RECOI l'objet de classe MainActivity en parametre
-		
+
 		sAdresse = Adresse;
 		iPortDrone = iPort;
 		CommandeGenere = CommandeStart;
 		mJoy = joyActivity;
-		
+
 
 		mThreads = new GereThreads(this);
-		
+
 		udp_socket = CreationSocketPacket(CommandeStart, iPort, Adresse); // CREATION DU SOCKET
 
 
 		NavRec = new NavdataReceiver(udp_socket); //
-		
+
 		mThreads.StartDonnee();
-		
-		
+
+
 		mThreads.StartWatchDog();
 
-		
+
+	}
+
+	/**
+
+	 *	This method created a socket and a packet. It is performed only once in
+
+the constructor of JoyActivity.
+
+ Indeed, two objects are required for UDP communication, a socket and a 
+
+packet as respective class DatagramSocket and DatagramPacket  (classes 
+
+available in the Android libraries).
+
+	@param ComandeUDP start command
+	@param port The Port to use
+	@param host The Address IP target 
+
+	 */
+	public DatagramSocket CreationSocketPacket(String ComandeUDP, int port, String host){
+		buffer = new byte[512];
+
+		try {											// TRY CREATION SOCKET
+			udp_socket = new DatagramSocket();
+
+
+		} catch (SocketException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+
+		}	
+
+
+
+		try {													// INSERTION TRAME
+			buffer = ComandeUDP.concat("\r").getBytes("ASCII");
+
+		} catch (UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+
+		}
+
+		try {												// INSERTION ADRESSE
+			adresse = InetAddress.getByName(host);
+		} catch (UnknownHostException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+
+		udp_packet = new DatagramPacket(buffer, buffer.length, adresse, port);
+		return udp_socket;
+
+	}
+
+	
+	/**
+
+	 *	This function simply receives the UDP frame to be sent and send it with 
+
+the socket and packet created with CreationSocketPacket (). It must be 
+
+executed in a thread.
+
+	 */
+	public void EnvoiTrameUDP(String ComandeUDP){	
+
+
+		try {																// INSERTION TRAME
+			buffer = ComandeUDP.concat("\r").getBytes("ASCII");
+
+		} catch (UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+
+		}
+		udp_packet.setData(buffer);							// Changement de la commande du packet
+
+		try {
+			udp_socket.send(udp_packet);
+
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+
+		}
+	}
+
+	/**
+
+	 *	This function resets the variables (chronometer sequence, flight’s
+
+Boolean ..) and then start Piloting threads, which use 
+
+EnvoiTrameUDP() function to send the AT command.
+
+	 */
+	public void decollage() { // BOUTTON DECOLLAGE, LANCE LE THREAD DE DECOLLAGE ET DE PILOTAGE
+
+
+		//	RelanceSocketThreadDonnee();
+
+
+		mThreads.bWatchDog = false;
+		iVal = 1;
+		iSecondeU = 0;
+		iSecondeD = 0;
+		iMinuteU = 0;
+		iMinuteD = 0;
+		icSecondeU = 0;
+		icSecondeD = 0;
+
+		AttenteMs(50);
+		mThreads.Startdecollage();
+
+		AttenteMs(50);
+		mThreads.StartPilotage();
+
+
+		mScheduleTaskExecutor.scheduleAtFixedRate(mThreads.mTaskChrono, 0, 10,TimeUnit.MILLISECONDS);
+
+
 	}
 	
-	   public DatagramSocket CreationSocketPacket(String ComandeUDP, int port, String host){
-	    	buffer = new byte[512];
-	    	 
-				try {											// TRY CREATION SOCKET
-					udp_socket = new DatagramSocket();
-					
-				
-				} catch (SocketException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-					
-				}	
-		
+	/**
 
-	    		
-				try {													// INSERTION TRAME
-					buffer = ComandeUDP.concat("\r").getBytes("ASCII");
+	 *	This function stops the steering threads using a boolean, it stops the
 
-				} catch (UnsupportedEncodingException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
+	timer and starts the Atterissage and Watchdog threads (the emergency landing 
 
-				}
-							
-				try {												// INSERTION ADRESSE
-					adresse = InetAddress.getByName(host);
-				} catch (UnknownHostException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-				}
-			
-			udp_packet = new DatagramPacket(buffer, buffer.length, adresse, port);
-			return udp_socket;
-							
-	    }
-	   
-	    public void EnvoiTrameUDP(String ComandeUDP){	
-	    	
-	    	
-	    	try {																// INSERTION TRAME
-				buffer = ComandeUDP.concat("\r").getBytes("ASCII");
+	function is the same, only the thread and thus the associated command 
 
-			} catch (UnsupportedEncodingException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+	change).
 
-			}
-	    	udp_packet.setData(buffer);							// Changement de la commande du packet
-	    	
-				try {
-				udp_socket.send(udp_packet);
-				
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-				
-			}
-	    }
-	    
-	    public void decollage() { // BOUTTON DECOLLAGE, LANCE LE THREAD DE DECOLLAGE ET DE PILOTAGE
-										 
-			
-	    	//	RelanceSocketThreadDonnee();
-									
-				
-				mThreads.bWatchDog = false;
-				iVal = 1;
-				iSecondeU = 0;
-				iSecondeD = 0;
-				iMinuteU = 0;
-				iMinuteD = 0;
-				icSecondeU = 0;
-				icSecondeD = 0;
-
-				AttenteMs(50);
-				mThreads.Startdecollage();
-		
-				AttenteMs(50);
-				mThreads.StartPilotage();
-				
-
-				mScheduleTaskExecutor.scheduleAtFixedRate(mThreads.mTaskChrono, 0, 10,TimeUnit.MILLISECONDS);
-			
-
-		}
-	    
-	    public void atterissage() { // ATTERIT ET ARRETE LE THREAD DE PILOTAGE, LE REMPLACE PAR LE WATCHDOG
-			 
-
-	    	mScheduleTaskExecutor.shutdownNow();
-	    	mScheduleTaskExecutor = Executors.newSingleThreadScheduledExecutor();
-	    	mThreads.bWatchDog = true;
-			
-			AttenteMs(50);
-		
-			
-			mThreads.StartAtterissage();
-			AttenteMs(50);
-			mThreads.StartWatchDog();
-			
-			
-			
+	 */
 	
-			
+	public void atterissage() { // ATTERIT ET ARRETE LE THREAD DE PILOTAGE, LE REMPLACE PAR LE WATCHDOG
 
-		
+
+		mScheduleTaskExecutor.shutdownNow();
+		mScheduleTaskExecutor = Executors.newSingleThreadScheduledExecutor();
+		mThreads.bWatchDog = true;
+
+		AttenteMs(50);
+
+
+		mThreads.StartAtterissage();
+		AttenteMs(50);
+		mThreads.StartWatchDog();
+
+
+
+
+
+
+
+
+	}
+
+	public void emergency() {				// boutton atterissage d'urgence
+
+		mScheduleTaskExecutor.shutdownNow();
+		mScheduleTaskExecutor = Executors.newSingleThreadScheduledExecutor();
+
+		mThreads.bWatchDog = true;
+
+		mThreads.StartEmergency();
+
+		AttenteMs(50);
+
+
+
+	}
+
+
+	public void AttenteMs(int iMSeconde) { // FONCTION UTILE D'ATTENTE NON BLOQUANTE
+
+
+		try {
+			TimeUnit.MILLISECONDS.sleep(iMSeconde);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+
+	public void IncrementSeq() { // INCREMENTE LA VARIABLE DE SEQUENCE
+		if (iVal >= 1000) {
+			iVal = 1;
+		} else {
+
+			iVal++;
 
 		}
 
-		public void emergency() {				// boutton atterissage d'urgence
+	}
 
-			mScheduleTaskExecutor.shutdownNow();
-			mScheduleTaskExecutor = Executors.newSingleThreadScheduledExecutor();
 
-			mThreads.bWatchDog = true;
-			
-			mThreads.StartEmergency();
-		
-			AttenteMs(50);
-
-			
-
-		}
-		
-	    
-	    public void AttenteMs(int iMSeconde) { // FONCTION UTILE D'ATTENTE NON BLOQUANTE
-			
-
-			try {
-				TimeUnit.MILLISECONDS.sleep(iMSeconde);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
-		
-		
-		public void IncrementSeq() { // INCREMENTE LA VARIABLE DE SEQUENCE
-			if (iVal >= 1000) {
-				iVal = 1;
-			} else {
-				
-				iVal++;
-
-			}
-
-		}
-
-	
 }
-	
-	
-	
+
+
+
 
 
